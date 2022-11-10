ca61003bec37a2d53c6fe3b6ec377c18ee396156
egoing <kmd1801@naver.com>
2022-11-10 20:18:33 +0900

    8주차 제출

--- Week 5/박경태/1981_배열에서 이동.py
+++ Week 5/박경태/1981_배열에서 이동.py
@@ -1,61 +1,44 @@
-def passable(maps, n) :
-    start = [0]
-    route = [0]
-    while True :
-        temp = []
-        movable = False
-        for s in start :
-            row, col = s // 100, s % 100
-            if maps[row][col] == 1 :
-                if row > 0 and maps[row-1][col] == 1 :
-                    num = (row-1)*100+col
-                    if num not in route :
-                        temp.append(num)
-                    movable = True
-                if row < n-1 and maps[row+1][col] == 1 :
-                    num = (row+1)*100+col
-                    if num not in route :
-                        temp.append(num)
-                    movable = True
-                if col > 0 and maps[row][col-1] == 1 :
-                    num = row*100+col-1
-                    if num not in route :
-                        temp.append(num)
-                    movable = True
-                if col < n-1 and maps[row][col+1] == 1 :
-                    num = row*100+col+1
-                    if num not in route :
-                        temp.append(num)
-                    movable = True
+def passable(maps, boundary, n) :
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    q = [[0, 0]]
+    temp = [[0 for _ in range(n)] for _ in range(n)]
+    temp[0][0] = 1
+    while len(q) >= 1 :
+        row, col = q.pop(0)
+        if temp[row][col] == 1 :
+            for i in range(4) :
+                d_row = row + dx[i]
+                d_col = col + dy[i]
+                if d_row < 0 or d_row > n-1 or d_col < 0 or d_col > n-1 or temp[d_row][d_col] == 1 :
+                    continue
+                if maps[d_row][d_col] in boundary :
+                    temp[d_row][d_col] = 1
+                    q.append([d_row, d_col])
 
-        if (n-1)*100 + n-1 in route :
+        if temp[n-1][n-1] == 1 :
             return True
-
-        if not movable :
-            return False
-
-        route.extend(temp)
-        start = temp
+        
+    return False
 
 n = int(input())
 maps = [[int(i) for i in input().split()] for j in range(n)]
 min_num = min([min(row) for row in maps])
 max_num = max([max(row) for row in maps])
-length = max_num - min_num
-start_num = maps[0][0]
-end_num = maps[n-1][n-1]
-for i in range(abs(start_num - end_num), length+1) :
-    for j in range(min(i, start_num - min_num)+1) :
-        top = i - j
-        boundary = range(start_num-j, start_num+top+1)
-        temp = [[1 if i in boundary else 0 for i in row]for row in maps]
-        if temp[n-1][n-1] == 0 :
-            continue
-
-        if passable(temp, n) :
-            print(i)
-            break
+start = maps[0][0]
+end = maps[n-1][n-1]
+l_max = min(start, end)
+r_min = max(start, end)
+l_min = min_num
+r_max = max_num
+answer = 300000
+left = l_min
+right = r_min
+while l_min <= left <= l_max and r_min <= right <= r_max :
+    boundary = range(left, right+1)
+    if passable(maps, boundary, n) :
+        answer = min(answer, right-left)
+        left += 1
     else :
-        continue
-
-    break
+        right += 1
+print(answer)
